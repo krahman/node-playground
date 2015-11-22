6e3c5825488616cdf911bdb9db96216892b1b460
Khalilur Rahman <khal.rahman@gmail.com>
2015-11-22 14:14:31 +0800

    add dynamic content with html template

--- http/server.js
+++ http/server.js
@@ -4,6 +4,8 @@ var fs = require('fs');
 http.createServer(function(req, res){
    res.writeHead(200, {'Content-Type' : 'text/html'});
    // Read synchronousely
-   var html = fs.readFileSync(__dirname + '/index.html');
+   var html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+   var content = 'Hello World!!!';
+   html = html.replace('{content}', content);
    res.end(html);
 }).listen(3000, '127.0.0.1');
