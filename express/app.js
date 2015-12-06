8fd83897ba77708873144d7c0c5ec05228672f99
Khalilur Rahman <khal.rahman@gmail.com>
2015-12-06 21:35:14 +0800

    Added local env configs & added mysql conn

--- express/app.js
+++ express/app.js
@@ -1,6 +1,27 @@
 var express = require('express');
 var app = express();
 
+var config = require('config');
+var dbConfig = config.get('Nodehack.dbConfig');
+var appConfig = config.get('Nodehack.appConfig');
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host     : dbConfig.host,
+	user     : dbConfig.user,
+	password : dbConfig.password,
+	database : dbConfig.database
+});
+
+connection.connect(function(err) {
+	if (err) {
+		console.error('Error connecting : ' + err.stack);
+		return;
+	}
+
+	console.log('Connected as id : ' + connection.threadId);
+});
+
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
 
@@ -17,4 +38,4 @@ app.use('/', function(req, res, next) {
 htmlController(app);
 apiController(app);
 
-app.listen(3000);
+app.listen(appConfig.port);
