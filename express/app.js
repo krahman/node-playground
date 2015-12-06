f53c6a7f4cabecadcb5a904726851dfc0581cdbe
Khalilur Rahman <khal.rahman@gmail.com>
2015-12-06 15:13:42 +0800

    added node body-parser middleware

--- express/app.js
+++ express/app.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonPrser = bodyParser.json();
+
 app.set('view engine', 'ejs');
 
 app.use('/assets', express.static(__dirname + '/public'));
+app.use('/lib', express.static(__dirname + '/bower_components'));
 
 app.use('/', function(req, res, next) {
 	console.log('Opening : ' + req.url);
@@ -16,6 +21,16 @@ app.get('/', function(req, res) {
 
 app.get('/person', function(req, res) {
 	res.json({firstName: 'John', lastName: 'Doe'});
+});
+
+app.post('/person', urlencodedParser, function(req, res) {
+	console.log({firstName: req.body.firstName, lastName: req.body.lastName});
+
+	res.send('Thank you : ' + req.body.firstName);
+});
+
+app.post('/personjson', jsonPrser, function(req, res) {
+	console.log({firstName: req.body.firstName, lastName: req.body.lastName});
 });
 
 app.get('/person/:id', function(req, res) {
