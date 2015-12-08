dd167758c40ef3b442612ec4baf239444507d9bf
Khalilur Rahman <khal.rahman@gmail.com>
2015-12-09 01:09:39 +0800

    installed cookie-parser & express-session for parse cookie and session middleware

--- express/app.js
+++ express/app.js
@@ -4,20 +4,34 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
-var config = require('config');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var morgan = require('morgan');
 
 var apiController = require('./controllers/apiController');
 var htmlController = require('./controllers/htmlController');
 
+var config = require('config');
 var connection = require('./db/connection')(config);
 
 app.set('view engine', 'ejs');
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(session({
+	secret: '[SECRET_STRING]',
+	saveUninitialized: true, 
+	resave: true
+}));
 
 app.use('/assets', express.static(__dirname + '/public'));
 app.use('/lib', express.static(__dirname + '/bower_components'));
 
 app.use('/', function(req, res, next) {
 	console.log('Opening : ' + req.url);
+	console.log(req.cookies);
+	console.log('*****************');
+	console.log(req.session);
 	next();
 });
 
